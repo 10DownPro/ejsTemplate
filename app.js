dfd82387ed1af7108de4b9e717a0f5f998d31cea
T'Vedt Lazenby <tvedt@10downproductions.com>
2022-10-04 17:12:33 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// Set express as Node.js web application
+
+// server framework.
+
+// To install express before using it as
+
+// an application server by using
+
+// "npm install express" command.
+
+var express = require('express');
+
+var app = express();
+
+ 
+
+// Set EJS as templating engine
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res)=>{
+
+ 
+
+    // The render method takes the name of the html
+    
+    // page to be rendered as input.
+    
+    // This page should be in views folder
+    
+    // in the root directory.
+    
+    var data = {name:'T\'Vedt',
+    
+        hobbies:['coding', 'reading', 'travel']};
+    
+     
+    res.render('home', {data:data});
+    
+    });
+    
+     
+    
+    var server = app.listen(4000, function() {
+    
+        console.log('listening to port 4000')
+    
+    });
